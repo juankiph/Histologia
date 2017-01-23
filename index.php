5d82c72f9be1ce8e269666cd00c8ec75f3f6039a
Manuel <Manuel@DESKTOP-KFFJULL>
2017-01-23 18:37:23 +0100

    Trabajo fin de grado. Primer commit. LETS GO

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>FlashCard</title>
+        <link rel ="stylesheet" href="css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Francois+One" rel="stylesheet">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <nav class="navbar navbar-inverse navbar-fixed-top">
+                <div class="container-fluid">
+                    <!--                    <div class="navbar-header">
+                                            <a class="navbar-brand" href="#">WebSiteName</a>
+                                        </div>
+                                        <ul class="nav navbar-nav">
+                                            <li class="active"><a href="#">Home</a></li>
+                                            <li><a href="#">Page 1</a></li>
+                                            <li><a href="#">Page 2</a></li>
+                                            <li><a href="#">Page 3</a></li>
+                                        </ul>-->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="login.php"><span class="glyphicon glyphicon-user"></span> Registrate</a></li>
+                        <li><a href="registrate.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <?php
+        echo "hola mundo"
+        ?>
+    </body>
+</html>
